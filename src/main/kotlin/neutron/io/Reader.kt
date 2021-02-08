67868377b3c6f32e920c04ce83de699f3e22cc81
Neutron17 <sandormatyas17@gmail.com>
2021-02-08 22:06:26 +0100

    Added Vector, ETC, Reader, Writer

--- src/main/kotlin/neutron/io/Reader.kt
+++ src/main/kotlin/neutron/io/Reader.kt
@@ -7,64 +7,64 @@ import java.io.FileNotFoundException
 import java.util.*
 
 class Reader {
-    companion object{
+    companion object {
         /** Read a single char from a file */
-        fun readSingleChar(path:String) {
-            try {
-                val fis = FileInputStream(path) // File path
-                val i = fis.read()
-                println(i.toChar())
-                fis.close()
-            } catch (ex: Exception) {
-                println("" + ex)
-            }
+    fun readSingleChar(path:String) {
+        try {
+            val fis = FileInputStream(path) // File path
+            val i = fis.read()
+            println(i.toChar())
+            fis.close()
+        } catch (ex: Exception) {
+            println("" + ex)
         }
-        /** Read multiple chars from a file */
-        fun readMultipleChar(path: String) {
-            try {
-                val fis = FileInputStream(path) // File path
-                var i = 0
-                var result = ""
-                while (fis.read().also { i = it } != -1) {
-                    result += i.toChar()
-                }
-                println(result)
-                fis.close()
-                println("Finished")
-            } catch (ex: Exception) {
-                println("" + ex)
+    }
+    /** Read multiple chars from a file */
+    fun readMultipleChar(path: String) {
+        try {
+            val fis = FileInputStream(path) // File path
+            var i = 0
+            var result = ""
+            while (fis.read().also { i = it } != -1) {
+                result += i.toChar()
             }
+            println(result)
+            fis.close()
+            println("Finished")
+        } catch (ex: Exception) {
+            println("" + ex)
         }
-        /** Read multiple chars with buffer */
-        fun bufferedReader(path: String) {
-            try {
-                val fis = FileInputStream(path) // File path
-                val bin = BufferedInputStream(fis)
-                var i = 0
-                var result = ""
-                while (bin.read().also { i = it } != -1) {
-                    result += i.toChar()
-                }
-                println(result)
-                fis.close()
-            } catch (ex: Exception) {
-                println("" + ex)
+    }
+    /** Read multiple chars with buffer */
+    fun bufferedReader(path: String) {
+        try {
+            val fis = FileInputStream(path); // File path
+            val bin = BufferedInputStream(fis);
+            var i = 0;
+            var result = "";
+            while (bin.read().also { i = it } != -1) {
+                result += i.toChar();
             }
+            println(result);
+            fis.close();
+        } catch (ex:Exception) {
+            System.out.println("" + ex);
         }
-        fun read(path:String): String {
-            try {
-                val file = File(path)
-                val sc = Scanner(file)
-                while (sc.hasNextLine()) {
-                    val data = sc.nextLine()
-                    println(data)
-                    return data
-                }
-                sc.close()
-            } catch (e: FileNotFoundException) {
-                e.printStackTrace()
+    }
+    fun read(path:String):String {
+        try {
+            val file = File(path);
+            val sc = Scanner(file);
+            while (sc.hasNextLine()) {
+                var data = sc.nextLine();
+                println(data);
+                return data;
             }
-            return ""
+            sc.close();
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace();
         }
+        return "";
+    }
     }
 }
